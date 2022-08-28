4bbd6bd54f6ae1d143d16c450194bbe5e5ea4a9e
smarkov2020 <markovsv777@mail.ru>
2022-08-28 11:03:08 +0300

    Коммит метода Win

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,30 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
-        return rsl;
+        boolean rsl = false;
+        for (int index = 0; index < board.length; index++) {
+            if (board[index][index] == 1 && checkHorisontalCondition(board, index) && checkVerticalCondition (board, index)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean checkHorisontalCondition (int[][] board, int row) {
+        for (int index = 0; index < board.length; index++) {
+            if (board[index][row] != 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkVerticalCondition (int[][] board, int cell) {
+        for (int index = 0; index < board.length; index++) {
+            if (board[cell][index] != 1) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
+++ puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
@@ -1,10 +1,11 @@
 package ru.job4j.puzzle;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.*;
+import org.junit.Ignore;
+import org.junit.Test;
 
-@Disabled("Тесты отключены. Удалить аннотацию после реализации метода check()")
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
 public class WinTest {
     @Test
     public void whenVerticalWin() {
@@ -15,7 +16,7 @@ public class WinTest {
                 {0, 0, 1, 0, 0},
                 {0, 0, 1, 0, 0},
         };
-        assertThat(Win.check(board)).isTrue();
+        assertThat(Win.check(board), is(true));
     }
 
     @Test
@@ -27,19 +28,19 @@ public class WinTest {
                 {0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0},
         };
-        assertThat(Win.check(board)).isTrue();
+        assertThat(Win.check(board), is(true));
     }
 
     @Test
-    public void whenNotWin() {
+    public void whenNotWinV() {
         int[][] board = {
+                {0, 0, 1, 0, 0},
+                {0, 1, 1, 0, 0},
                 {0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0},
-                {1, 1, 1, 1, 0},
-                {0, 0, 0, 1, 0},
-                {0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
         };
-        assertThat(Win.check(board)).isFalse();
+        assertThat(Win.check(board), is(false));
     }
 
     @Test
@@ -51,6 +52,6 @@ public class WinTest {
                 {0, 0, 0, 1, 0},
                 {1, 1, 1, 1, 0},
         };
-        assertThat(Win.check(board)).isFalse();
+        assertThat(Win.check(board), is(false));
     }
 }
