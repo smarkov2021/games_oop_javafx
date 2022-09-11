3fac84b5a3bc400263357783dcaa71cb599d9245
smarkov2020 <markovsv777@mail.ru>
2022-09-11 18:52:43 +0300

    Коммит метода Win

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -4,15 +4,14 @@ public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = false;
         for (int i = 0; i < board.length; i++) {
-            if (board[i][i] == 1 && (checkHorisontalCondition(board, i) ||
-                    checkVerticalCondition(board, i))) {
+            if (board[i][i] == 1 && (checkHorisontal(board, i) || checkVertical(board, i))) {
                 return true;
             }
         }
         return false;
     }
 
-    public static boolean checkHorisontalCondition(int[][] board, int row) {
+    public static boolean checkHorisontal(int[][] board, int row) {
         for (int i = 0; i < board.length; i++) {
             if (board[i][row] != 1) {
                 return false;
@@ -21,7 +20,7 @@ public class Win {
         return true;
     }
 
-    public static boolean checkVerticalCondition(int[][] board, int cell) {
+    public static boolean checkVertical(int[][] board, int cell) {
         for (int i = 0; i < board.length; i++) {
             if (board[cell][i] != 1) {
                 return false;
